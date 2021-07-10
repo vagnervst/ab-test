34d046f4f03ba1dce544104ba8282d4935586f8e
Vagner S <vagners.tambalo@gmail.com>
2021-07-10 15:54:51 -0300

    pages(home): add button component

---
+++ src/components/Button.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+const Base = styled.button`
+  background: none;
+  border: none;
+  padding: 8px;
+  border-radius: 2px;
+  font-weight: bold;
+  cursor: pointer;
+`
+
+const Solid = styled(Base)`
+  background-color: #dd0939;
+  color: #fff;
+  transition: background-color .1s;
+
+  &:hover {
+    background-color: #f62252;
+  }
+
+  &:active {
+    background-color: #ad032c;
+  }
+`
+
+const Outline = styled(Base)`
+  border: 2px solid #141414;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, .25);
+  }
+
+  &:active {
+    background-color: #141414;
+    color: #fff;
+  }
+`
+
+const Button = ({ children, variant }) => {
+  const Variant = variant === 'solid'
+    ? Solid
+    : Outline
+
+  return (
+    <Variant>{children}</Variant>
+  )
+}
+
+export default Button
