a8a37d09b1ca00da46632f04d322c91c44e5bfde
Vagner S <vagners.tambalo@gmail.com>
2021-07-12 16:32:42 -0300

    page: add datalayer variable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { useEffect } from 'react'
+
 import Button from './components/Button'
 
-const App = () => (
-  <div>
-    <p>
-      This page has two variants of the same button that are presented
-      exclusively according to Google optimize's variant configuration.
-    </p>
-    <Button variant="solid">Click Me!</Button>
-    <Button style={{ display: 'none' }} variant="outline">Click Me!</Button>
-  </div>
-)
+const App = () => {
+  useEffect(() => {
+    const { dataLayer } = window
+    const users = Number(localStorage.getItem('users')) || 0
+    console.log({ users })
+
+    dataLayer.push({ users })
+    dataLayer.push({ 'event': 'optimize.activate' })
+  }, [])
+
+  return (
+    <div>
+      <p>
+        This page has two variants of the same button that are presented
+        exclusively according to Google optimize's variant configuration.
+      </p>
+      <Button variant="solid">Click Me!</Button>
+      <Button style={{ display: 'none' }} variant="outline">Click Me!</Button>
+    </div>
+  )
+}
 
 export default App;
