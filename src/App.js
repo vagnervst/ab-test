7b7400a4e13db4b9ad0d44604b92aaf23ac856fd
Vagner S <vagners.tambalo@gmail.com>
2021-07-29 12:35:42 -0300

    feat: add window variable segmentation

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import Button from './components/Button'
 
 const App = () => {
   useEffect(() => {
-    const { dataLayer } = window
+    const { dataLayer, gtag } = window
     const users = Number(localStorage.getItem('users')) || 0
     console.log({ users })
 
     dataLayer.push({ users })
-    dataLayer.push({ 'event': 'optimize.activate' })
+    dataLayer.push({ event: 'optimize.activate' })
+
+    gtag('event', 'optimize.callback', {
+      name: process.env.REACT_APP_EXPERIMENT_ID,
+      callback: () => {
+        console.log('experiment passed')
+      }
+    })
   }, [])
 
   return (
@@ -18,8 +25,11 @@ const App = () => {
         This page has two variants of the same button that are presented
         exclusively according to Google optimize's variant configuration.
       </p>
-      <Button variant="solid">Click Me!</Button>
-      <Button style={{ display: 'none' }} variant="outline">Click Me!</Button>
+      {window.isSegmented ? (
+          <Button variant="outline">Click Me!</Button>
+        ) : (
+          <Button variant="solid">Click Me!</Button>
+      )}
     </div>
   )
 }
